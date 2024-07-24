207218a0948d32a0caac93a5754e0447cd4fc66f
Lucas Lima Brito <lucas_lima_brito@hotmail.com>
2024-07-24 18:40:48 -0300

    primeiro get e post com django

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from genres.views import genre_create_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('genres/',genre_create_list_view,name='genre_create-list'),
 ]
